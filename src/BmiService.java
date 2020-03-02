3b71709b57fa9e9bddb6bf7b8663a498b9adb618
AntonZ-Frank <toni.rus@yandex.ru>
2020-03-02 20:13:10 +0500

    2 commit

---
+++ src/BmiService.java
@@ -0,0 +1,10 @@
+public class BmiService {
+    public double calculate (double total)
+    {
+        double weight = 60; //кг
+        double height = 180; // см
+        double index = weight / ((height / 100) * (height / 100));
+        return index;
+
+    }
+}
