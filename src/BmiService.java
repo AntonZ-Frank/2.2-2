69682c0acdc318ad348d69518cd3e4d7a18fbdd3
AntonZ-Frank <toni.rus@yandex.ru>
2020-03-05 18:55:26 +0500

    форматирование кода

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,8 @@
 public class BmiService {
-    public double calculate (double total)
-    {
+    public double calculate(double total) {
         double weight = 60; //кг
         double height = 180; // см
         double index = weight / ((height / 100) * (height / 100));
         return index;
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         BmiService service = new BmiService();
         double total = 0;
         double index = service.calculate(total);
